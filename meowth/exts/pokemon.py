8baa6ff236127e62ab7751b377ed55b337b7ed9c
BrenenP <xdooncex@gmail.com>
2020-02-26 15:04:36 -0500

    form AND gender support.

--- meowth/exts/pokemon.py
+++ meowth/exts/pokemon.py
@@ -357,6 +357,7 @@ class Pokemon():
         shiny_str = ""
         shadow_str = ""
         region_list = ["alolan", "galarian"]
+        form_list = [x for x in list(self.bot.pkmn_info[self.name.lower()]['forms'].keys()) if x not in region_list]
         if self.gender == "female":
             gender_str = "_01"
         elif self.gender == "male":
@@ -366,7 +367,6 @@ class Pokemon():
         elif self.region == "galarian":
             region_str = "_31"
         if self.form:
-            form_list = [x for x in list(self.bot.pkmn_info[self.name.lower()]['forms'].keys()) if x not in region_list]
             form_index = form_list.index(f"{self.region+' ' if self.region else ''}{str(self.form).lower()}")
             form_str = f"{region_str}_{str(form_index).zfill(2)}"
         elif self.region:
@@ -377,6 +377,8 @@ class Pokemon():
             shadow_str = f"_{self.shadow}"
         if not self.gender and not self.form and not self.region:
             form_str = "_00"
+        if not self.gender and self.bot.pkmn_info[self.name.lower()]['forms'][f"{self.region+' ' if self.region else ''}{str(self.form).lower()}"].get('gender') and len(form_list) > 1:
+            gender_str = "_00"
         return (f"https://raw.githubusercontent.com/doonce/Meowth/Rewrite/images/pkmn_icons/pokemon_icon_{pkmn_no}{gender_str}{form_str}{shiny_str}.png?cache=2")
 
     @property
